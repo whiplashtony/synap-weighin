0ecd77262a27130d06948a0f071b070b5486fb0f
Bryan Abbott <bryan@abbottps.com>
2018-06-06 21:39:48 -0500

    import csv via seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+CSV.foreach('participants.csv', :headers => true, :col_sep => ',') do |row|
+  Person.find_or_create_by(:name => row[0])
+  Event.find_or_create_by(:name => row[1], :created_at => row[3])
+end
+
+CSV.foreach('weighins.csv', :headers => true, :col_sep => ',') do |row|
+  Checkin.create( :weight => row[1], :person_id => Person.find_by_name(row[0]).id, :created_at => row[2], :event_id => Event.find_by_created_at(row[2].to_date).id )
+end
