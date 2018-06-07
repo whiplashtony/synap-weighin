f8402c4c823e6beee07d63f7aa0d6c950e388031
Bryan Abbott <bryan@abbottps.com>
2018-06-07 07:34:02 -0500

    league migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123070943) do
+ActiveRecord::Schema.define(version: 20180607123008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20171123070943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "tagline"
+  end
+
+  create_table "leagues", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "locations", force: true do |t|
